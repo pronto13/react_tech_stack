7af83f2e5e9ed8d5ef51887448301fc5a8133213
pronto13 <marasi.ae@gmail.com>
2018-09-25 17:53:26 +0700

    first

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+import { Header } from './components/common';
+import LibraryList from './components/LibraryList';
+
+const App = () => {
+  return (
+    // Provider works with Store
+    // Provider translates state in a store for React to use
+    // Provider can only have 1 children components
+    <Provider store={createStore(reducers)}>
+      <View style={{ flex: 1 }}>
+        <Header headerText="Tech Stack" />
+        <LibraryList />
+      </View>
+    </Provider>
+  );
+};
+
+export default App;
---
+++ src/components/common/index.js
@@ -0,0 +1,7 @@
+//import everything and export
+export * from './Button';
+export * from './Card';
+export * from './CardSection';
+export * from './Header';
+export * from './Input';
+export * from './Spinner';
